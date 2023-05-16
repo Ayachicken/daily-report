7e59dbe6e0394811239c520b2b1c90c90f95ccf5
Hari <a-harigae@mouse-jp.co.jp>
2023-05-16 15:03:57 +0900

    [add]ブラウザ上全範囲コピー

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-import xlwings as xw
 import config
+import xlwings as xw
+import os
+import time
+import datetime
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+load_dotenv()
+
+spfa_id = os.environ.get("USER_ID")
+spfa_pass = os.environ.get("PASS_WORD")
+repair_excel = os.environ.get("REPAIR_EXCEL")
+spfa_top = os.environ.get("SPFA_TOP")
+res_calc = os.environ.get("RES_CALC")
+calc_excel = os.environ.get("CALC_EXCEL")
+
+today = datetime.date.today().strftime("%Y/%m/%d")
 
 wb = xw.Book(repair_excel)
 
@@ -10,7 +29,7 @@ stf_name = sht.range('C694').value
 #.replace('　',' ')C694の氏名の全角スペースを半角に変換し、nameに格納
 
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install()) #Chromeを開く
-driver.get(spfa_top) #SPFAを開く
+driver.get(spfa_top) #社内ツールを開く
 time.sleep(3)
 
 user_name = driver.find_element(By.NAME,'USER_NAME') #ログインID入力欄に移動
@@ -36,7 +55,22 @@ time.sleep(5)
 driver.find_element(By.LINK_TEXT, stf_name).click()
 time.sleep(5)
 
+ActionChains(driver)\
+    .key_down(keys.CONTROL)\
+    .send_keys("a")\
+    .perform() #Ctrl+aで全範囲選択
 
+ActionChains(driver)\
+    .key_down(key.CONTROL)\
+    .send_keys("c")\
+    .perform #Ctrl+cでコピー
+
+calc_wb = xw.Book(calc_excel)
+
+sht = calc_wb['貼付シート']
+sht.activate() #貼付用シートをアクティブにする
+
+xw.Range('A5').paste
 
 xl = xw.apps.active.api
 xl.Quit()
